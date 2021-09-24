383b2858558e0511f1338e3eb5b876fb87a746c8
awongCM <awong.cm@gmail.com>
2021-09-24 17:45:43 +1000

    get the celery worker task running

--- api.py
+++ api.py
@@ -1,9 +1,11 @@
 import falcon
 
-from resources import BaseResource
+from resources import BaseResource, WorkerResource
 
 api = falcon.App()
 
 base = BaseResource()
+worker = WorkerResource()
 
 api.add_route('/', base)
+api.add_route('/start_worker', worker)
--- resources.py
+++ resources.py
@@ -2,6 +2,8 @@ import json
 
 import falcon
 
+from tasks import fib
+
 
 class BaseResource:
 
@@ -13,3 +15,21 @@ class BaseResource:
         resp.text = json.dumps(doc, ensure_ascii=False)
 
         resp.status = falcon.HTTP_200
+
+
+class WorkerResource():
+
+    def on_post(self, req, resp):
+
+        raw_json = req.stream.read()
+        result = json.loads(raw_json)
+        task = fib.delay(int(result['number']))
+        resp.status = falcon.HTTP_200
+        result = {
+            'status': 'success',
+            'data': {
+                'task_id': task.id
+            }
+        }
+
+        resp.text = json.dumps(result)
