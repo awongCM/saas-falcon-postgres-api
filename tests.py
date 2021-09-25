fadad79c05eea14ee345ed73fd470f9cf1fd3824
awongCM <awong.cm@gmail.com>
2021-09-25 17:02:39 +1000

    added tests and mocks for celery worker tasks

--- tests.py
+++ tests.py
@@ -1,7 +1,11 @@
 
+import unittest
+from unittest.mock import patch
+
 from falcon import testing
 
 from api import api as app
+import tasks
 
 
 class TestAPIRoute(testing.TestCase):
@@ -16,3 +20,24 @@ class TestAPIRoute(testing.TestCase):
         }
         result = self.simulate_get('/')
         self.assertEqual(result.json, expected)
+
+
+class TestWorkerTasks(testing.TestCase):
+    def setUp(self):
+        super(TestWorkerTasks, self).setUp()
+        self.app = app
+
+    @patch('tasks.fib')
+    def test_mock_tasks_fib(self, mock_fib):
+        mock_fib.run.return_value = []
+        self.assertEqual(tasks.fib.run(-1), [])
+        mock_fib.run.return_value = [0, 1, 1]
+        self.assertEqual(tasks.fib.run(2), [0, 1, 1])
+        mock_fib.run.return_value = [0, 1, 1, 2, 3]
+        self.assertEqual(tasks.fib.run(4), [0, 1, 1, 2, 3])
+        mock_fib.run.return_value = [0, 1, 1, 2, 3, 5]
+        self.assertEqual(tasks.fib.run(5), [0, 1, 1, 2, 3, 5])
+
+
+if __name__ == '__main__':
+    unittest.main()
